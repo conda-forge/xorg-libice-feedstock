638fb1a685dc801140e7b7b02cc76064f63868c0
Peter Williams <peter@newton.cx>
2017-02-02 22:36:15 -0500

    Update for new xorg-util-macros and try to fix the ws2_32 libtool issue

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,11 +6,6 @@ IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
 # Adopt a Unix-friendly path if we're on Windows (see bld.bat).
 [ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
 
-# Fresh OS-guessing scripts from xorg-util-macros for win64
-for f in config.guess config.sub ; do
-    cp -p $PREFIX/share/util-macros/$f .
-done
-
 if [ -n "$VS_MAJOR" ] ; then
     # Need to regenerate configure scripts to properly detect msys2.
     am_version=1.15 # keep sync'ed with meta.yaml
@@ -20,9 +15,15 @@ if [ -n "$VS_MAJOR" ] ; then
         --force
         --install
         -I "$PREFIX/share/aclocal"
+        -I "$LIBRARY_PREFIX/share/aclocal"
         -I "/mingw-w64/share/aclocal" # note: this is correct for win32 also!
     )
     autoreconf "${autoreconf_args[@]}"
+
+    # And we need to add the search path that lets libtool find the
+    # msys2 stub libraries for ws2_32.
+    platlibs=$(cd $(dirname $(gcc --print-prog-name=ld))/../lib && pwd)
+    export LDFLAGS="$LDFLAGS -L$platlibs"
 fi
 
 export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
